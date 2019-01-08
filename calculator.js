73e7e2bf5b66056642093963061bcd0cee1e75d4
guru3456 <36559265+guru3456@users.noreply.github.com>
2019-01-08 09:58:02 +0530

    calculator scripts

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function disp(val)
+{
+    document.getElementById("display").value+=val;
+}
+function solve()
+{
+    let a=document.getElementById("display").value;
+    let b= eval(a);
+    document.getElementById("display").value = b;
+}
+function clr()
+{
+    document.getElementById("display").value = "";
+}
